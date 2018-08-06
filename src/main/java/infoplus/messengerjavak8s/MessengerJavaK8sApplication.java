2358b39d62ea7d66af56bbc1ec4d9a10ebad4633
marstone <i@marstone.net>
2018-08-07 03:34:26 +0800

    create spring boot actuator messenger

---
+++ src/main/java/infoplus/messengerjavak8s/MessengerJavaK8sApplication.java
@@ -0,0 +1,12 @@
+package infoplus.messengerjavak8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerJavaK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerJavaK8sApplication.class, args);
+	}
+}
